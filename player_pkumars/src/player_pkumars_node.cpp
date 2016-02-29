9d195a5feb4ab27203430a75abe88ccd4d4aac67
prasanna kumar routray <prasanna.routray07@gmail.com>
2016-02-29 19:09:33 +0000

    modified

--- player_pkumars/src/player_pkumars_node.cpp
+++ player_pkumars/src/player_pkumars_node.cpp
@@ -48,6 +48,41 @@ class Player
 	//A public atribute
 	string name; // existing or declared name
 	
+	void getDistanceToAllHunters(void)
+	{
+		//for hunter in hunters 
+
+	}
+	
+	double getDistance(Player& p)
+	{
+		//computing the distance 
+		string first_refframe = p.name;
+		string second_refframe = name;
+
+		ros::Duration(0.1).sleep(); //To allow the listener to hear messages
+		tf::StampedTransform st; //The pose of the player
+		try{
+			listener.lookupTransform(first_refframe, second_refframe, ros::Time(0), st);
+		}
+		catch (tf::TransformException& ex){
+			ROS_ERROR("%s",ex.what());
+			ros::Duration(1.0).sleep();
+		}
+
+		tf::Transform t;
+		t.setOrigin(st.getOrigin());
+		t.setRotation(st.getRotation());
+
+		double x = t.getOrigin().x();
+		double y = t.getOrigin().y();
+
+		double norm = sqrt(x*x + y*y);
+		return norm;
+
+	}
+	
+	
 	//Gets team name and returns it(accessor) (because this team variable is private and cannot be used outside this class.)
 	string getTeamName(void)
 	{
@@ -81,85 +116,18 @@ class Player
 	
 	
 	private:
-	string team;
-	tf::TransformListener listener; //gets transforms from the system
-
-};
-
-//Class myPlayer extends class Player (inheritance)
-/**
- * @brief MyPlayer extends class Player, i.e., there are additional things I can do with MyPlayer and not with any Player, e.g., to order a movement.
- */
-class MyPlayer: public Player
-{
-    public: 
-
-		/**
-		 * @brief The transform publisher object
-		 */
-		tf::TransformBroadcaster br;
-
-		/**
-		 * @brief Constructor
-		 *
-		 * @param name player name
-		 * @param team team name
-		 */
-	
-    MyPlayer(string name, string team): Player(name) // coming name assigned here 
-    {
-        setTeamName(team);
-
-		//Initialize position to 0,0,0
-		tf::Transform t;
-		t.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
-		tf::Quaternion q; q.setRPY(0, 0, 0);
-		t.setRotation(q);
-		br.sendTransform(tf::StampedTransform(t, ros::Time::now(), "/map", name));
-    }
-    
-    /**
-	 * @brief Moves MyPlayer
-	 *
-	 * @param displacement the liner movement of the player, bounded by [-0.1, 1]
-	 * @param turn_angle the turn angle of the player, bounded by  [-M_PI/60, M_PI/60]
+	/**
+	 * @brief The name of the team
 	 */
-	void move(double displacement, double turn_angle)
-            {
-                //Put arguments withing authorized boundaries
-                double max_d =  1; 
-                displacement = (displacement > max_d ? max_d : displacement); // turnary operator.
+	string team;
 
-                double min_d =  -0.1; 
-                displacement = (displacement < min_d ? min_d : displacement);
+	/**
+	 * @brief the transform listener object
+	 */
+	tf::TransformListener listener; //reads tfs from the ros system
 
-                double max_t =  (M_PI/60);
-                if (turn_angle > max_t)
-                    turn_angle = max_t;
-                else if (turn_angle < -max_t)
-                    turn_angle = -max_t;
-
-                //Compute the new reference frame
-                tf::Transform t_mov;
-                t_mov.setOrigin( tf::Vector3(displacement , 0, 0.0) );
-                tf::Quaternion q;
-                q.setRPY(0, 0, turn_angle);
-                t_mov.setRotation(q);
-
-/**
- *  we are not publishing map.
- *  we are publishing only transformations 
- * 
- * 
- * */
-
-                tf::Transform t = getPose();
-                t = t  * t_mov;
-
-                //Send the new transform to ROS
-                br.sendTransform(tf::StampedTransform(t, ros::Time::now(), "/map", name));
-            }
 };
+
 
 /**
  * @brief this Contains a list of all the players on a team
@@ -168,6 +136,13 @@ class MyPlayer: public Player
 class Team
 {
     public: 
+    
+    /**
+	 * @brief Constructor
+	 *
+	 * @param team the team name
+	 * @param player_names a list with the name of all the players on the team
+	 */
 
     //Team constructor
     Team(string team, vector<string>& player_names)
@@ -204,6 +179,94 @@ class Team
 	vector<boost::shared_ptr<Player> > players;
 };
 
+
+
+
+
+//Class myPlayer extends class Player (inheritance)
+/**
+ * @brief MyPlayer extends class Player, i.e., there are additional things I can do with MyPlayer and not with any Player, e.g., to order a movement.
+ */
+class MyPlayer: public Player
+{
+    public: 
+
+		/**
+		 * @brief The transform publisher object
+		 */
+		tf::TransformBroadcaster br;
+		
+	    //vector<string> prey_names;
+		//prey_names.push_back("lalmeida");
+		//rws2016_moliveira::Team prey_team("green", prey_names);
+		//prey_team.printTeamInfo();
+
+		boost::shared_ptr<Team> prey_team;
+
+		/**
+		 * @brief Constructor
+		 *
+		 * @param name player name
+		 * @param team team name
+		 */
+	
+    MyPlayer(string name, string team): Player(name) // coming name assigned here 
+    {
+        setTeamName(team);
+
+		//Initialize position to 0,0,0
+		tf::Transform t;
+		t.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+		tf::Quaternion q; q.setRPY(0, 0, 0);
+		t.setRotation(q);
+		br.sendTransform(tf::StampedTransform(t, ros::Time::now(), "/map", name));
+    
+		vector<string> prey_names;
+		prey_names.push_back("lalmeida");
+		prey_team = (boost::shared_ptr<Team>) new Team("green", prey_names);
+    }
+    
+    /**
+	 * @brief Moves MyPlayer
+	 *
+	 * @param displacement the liner movement of the player, bounded by [-0.1, 1]
+	 * @param turn_angle the turn angle of the player, bounded by  [-M_PI/60, M_PI/60]
+	 */
+	void move(double displacement, double turn_angle)
+            {
+                //Put arguments withing authorized boundaries
+                double max_d =  1; 
+                displacement = (displacement > max_d ? max_d : displacement); // turnary operator.
+
+                double min_d =  -0.1; 
+                displacement = (displacement < min_d ? min_d : displacement);
+
+                double max_t =  (M_PI/60);
+                if (turn_angle > max_t)
+                    turn_angle = max_t;
+                else if (turn_angle < -max_t)
+                    turn_angle = -max_t;
+
+                //Compute the new reference frame
+                tf::Transform t_mov;
+                t_mov.setOrigin( tf::Vector3(displacement , 0, 0.0) );
+                tf::Quaternion q;
+                q.setRPY(0, 0, turn_angle);
+                t_mov.setRotation(q);
+
+/**
+ *  we are not publishing map.
+ *  we are publishing only transformations 
+ * */
+                tf::Transform t = getPose();
+                t = t  * t_mov;
+
+                //Send the new transform to ROS
+                br.sendTransform(tf::StampedTransform(t, ros::Time::now(), "/map", name));
+            }
+};
+
+
 }// namespace rws2016_pkumars ends here 
 
 
@@ -225,7 +288,21 @@ int main(int argc, char **argv)
     ros::NodeHandle node;
 
     //Creating an instance of class MyPlayer
-    rws2016_pkumars::MyPlayer my_player("pkumars", "red");
+    rws2016_pkumars::MyPlayer my_player("pkumars", "green");
+    
+    //vector<string> myteam_names;
+    //myteam_names.push_back("moliveira");
+    //rws2016_moliveira::Team my_team("red", myteam_names);
+    //my_team.printTeamInfo();
+
+    //vector<string> prey_names;
+    //prey_names.push_back("lalmeida");
+    //rws2016_moliveira::Team prey_team("green", prey_names);
+    //prey_team.printTeamInfo();
+    
+    rws2016_pkumars::Player lalmeida_player("lalmeida");
+    
+    
     
 
     //Infinite loop
@@ -236,10 +313,12 @@ int main(int argc, char **argv)
         tf::Transform t = my_player.getPose();
         cout << "x = " << t.getOrigin().x() << " y = " << t.getOrigin().y() << endl;
         
+        double dist_from_my_player_to_lalmeida = my_player.getDistance(lalmeida_player);
+        cout << "dist_from_my_player_to_lalmeida = " << dist_from_my_player_to_lalmeida << endl;
+        
         //Test the move method
         my_player.move(0.1, -M_PI/6);
         
-
         ros::spinOnce();
         loop_rate.sleep();
     }
