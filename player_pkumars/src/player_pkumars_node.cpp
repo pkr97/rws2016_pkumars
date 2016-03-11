d82c5b6474d831970d6075ff50f209e75f9d2dde
prasanna kumar routray <prasanna.routray07@gmail.com>
2016-03-11 15:31:12 +0000

    Modified

--- player_pkumars/src/player_pkumars_node.cpp
+++ player_pkumars/src/player_pkumars_node.cpp
@@ -413,18 +413,18 @@ class MyPlayer: public Player
 
                 //Step 1
                 string closest_prey = getNameOfClosestPrey();
-                string closest_hunter = getNameOfClosestHunter();
+                //string closest_hunter = getNameOfClosestHunter();
                 
-                ROS_INFO("Closest prey is %s", closest_prey.c_str());
-                ROS_INFO("Closest prey is %s", closest_hunter.c_str());
+                //ROS_INFO("Closest prey is %s", closest_prey.c_str());
+                //ROS_INFO("Closest prey is %s", closest_hunter.c_str());
 
                 //Step 2
                 double angle_prey = getAngle(closest_prey);
-                double angle_hunter = getAngle(closest_prey);
+                //double angle_hunter = getAngle(closest_prey);
 
                 //Step 3
                 double displacement = msg.cat; //I am a cat, others may choose another animal
-				double distance_hunter = getHunterDistance(closest_hunter);
+				//double distance_hunter = getHunterDistance(closest_hunter);
 				
 				
 				move(displacement, angle_prey);
